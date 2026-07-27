532e9e0574054a89583f30f3a243eb3fe8dfa9b6
Musharraf-Mahad <musharafmahad17@gmail.com>
2026-07-27 17:09:06 +0300

    submit exercise-3

---
+++ Exercise-3/src/App.jsx
@@ -0,0 +1,24 @@
+  import { UserCard } from "./UserCard";
+     const App = () => {
+      return (
+        <>
+        <UserCard 
+        name = "Musharraf"
+        email = "musharafmahad17@gmail.com"
+        />
+
+         <UserCard 
+        name = "Hassan"
+        email = "hassan@gmail.com"
+        />
+
+         <UserCard 
+        name = "Fatah"
+        email = "fatahyare@gmail.com"
+        />
+
+        </>
+      )
+    }
+
+    export default App;
---
+++ Exercise-3/src/UserCard.jsx
@@ -0,0 +1,10 @@
+
+    // Destructing the props
+    export const UserCard = ({name, email}) => {
+        return(
+            <div>
+                <h4>Name: {name}</h4>
+                <h4>Email: {email}</h4>
+            </div>
+        )
+    }
