65cc273af5dd9904d53823d891690a0bdedf8da5
Musharraf-Mahad <musharafmahad17@gmail.com>
2026-07-27 14:52:32 +0300

    submit exercise-1

---
+++ Exercise-1/src/App.jsx
@@ -0,0 +1,10 @@
+  import { UserCard } from "./UserCard";
+     const App = () => {
+      return (
+       <div>
+        <UserCard />
+       </div>
+      )
+    }
+
+    export default App;
---
+++ Exercise-1/src/UserCard.jsx
@@ -0,0 +1,9 @@
+
+    export const UserCard = () => {
+        return(
+            <div>
+                <h2>Musharraf Mahad</h2>
+                <p>musharafmahad17@gmail.com</p>
+            </div>
+        )
+    }
