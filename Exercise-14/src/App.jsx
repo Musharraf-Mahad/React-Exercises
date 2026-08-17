e55e5f805ce470507ccab721b514795ade90d8aa
Musharraf-Mahad <musharafmahad17@gmail.com>
2026-08-17 15:37:57 +0300

    submit exercise-14

---
+++ Exercise-14/src/App.jsx
@@ -0,0 +1,10 @@
+import { MultiStepForm } from "./MultiStepForm";
+     const App = () => {
+      return (
+        <>
+        <MultiStepForm />
+        </>
+      )
+    }
+
+    export default App;
