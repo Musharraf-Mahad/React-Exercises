4a48a742e345a94151b7757cc689c95feb15bbc1
Musharraf-Mahad <musharafmahad17@gmail.com>
2026-08-23 15:43:09 +0300

    submit exercie-18

---
+++ Exercise-18/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ContactApp from './ContactApp'
+const App = () => {
+  return (
+    <ContactApp />
+  )
+}
+
+export default App
---
+++ Exercise-18/src/reducer.js
@@ -0,0 +1,26 @@
+export const initialState = [];
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      return [...state, action.payload];
+    case "edit":
+      return state.map((contact) =>
+        contact.id === action.payload.id ? action.payload : contact,
+      );
+    case "delete":
+      return state.filter((contact) =>
+        action.payload ? contact.id !== action.payload : contact,
+      );
+
+    case "toggleFavorite":
+      return state.map((contact) =>
+        contact.id === action.payload
+          ? { ...contact, favorite: !contact.favorite }
+          : contact,
+      );
+
+    default:
+      return state;
+  }
+};
